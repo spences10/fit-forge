601b15e25855e232438b178fbe80fc5d4620ad49
spences10 <spences10apps@gmail.com>
2023-10-29 17:13:49 +0000

    chore: :art: run prettier w/config

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,4 +1,7 @@
-export const calculate_bmi = (weight_kg: number, height_cm: number) => {
+export const calculate_bmi = (
+	weight_kg: number,
+	height_cm: number
+) => {
 	const height_m = height_cm / 100;
 	const bmi = weight_kg / (height_m * height_m);
 	return bmi.toFixed(1);
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,15 @@
-const daisyui = require("daisyui");
-const typography = require("@tailwindcss/typography");
+const daisyui = require('daisyui');
+const typography = require('@tailwindcss/typography');
 
 /** @type {import('tailwindcss').Config}*/
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
-  theme: {
-    extend: {}
-  },
+	theme: {
+		extend: {},
+	},
 
-  plugins: [typography, daisyui]
+	plugins: [typography, daisyui],
 };
 
 module.exports = config;
